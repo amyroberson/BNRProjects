3d500815dbae446ec66095c367a75fa924e0cec3
Amy Roberson <amy@roberson.xyz>
2017-04-03 21:02:45 -0400

    updated vocal editor

--- VocalEditor/VocalEditor/ViewController.swift
+++ VocalEditor/VocalEditor/ViewController.swift
@@ -8,24 +8,47 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-
+class ViewController: NSViewController, NSSpeechSynthesizerDelegate {
+    
     let speechSynthesizer = NSSpeechSynthesizer()
     
+    var contents: String? {
+        get {
+            return textView.string
+        }
+        set {
+            textView.string = newValue
+        }
+    }
+    
     @IBOutlet var textView: NSTextView!
+    @IBOutlet var speakButton: NSButton!
+    @IBOutlet var stopButtton: NSButton!
+    
     
     @IBAction func speakButtonClicked(_ sender: NSButton) {
-        if let contents = textView.string, !contents.isEmpty {
-            speechSynthesizer.startSpeaking(contents)
-        } else {
-            speechSynthesizer.startSpeaking("This Document is empty.")
-        }
+
+            if let contents = textView.string, !contents.isEmpty {
+                speechSynthesizer.startSpeaking(contents)
+            } else {
+                speechSynthesizer.startSpeaking("This Document is empty.")
+            }
+        
     }
     
     
     @IBAction func stopButtonClickerd(_ sender: NSButton) {
         speechSynthesizer.stopSpeaking()
     }
-
+    
+    func speechSynthesizer(_ sender: NSSpeechSynthesizer, didFinishSpeaking finishedSpeaking: Bool) {
+        if !finishedSpeaking{
+            speakButton.isEnabled = false
+            stopButtton.isEnabled = true
+        } else {
+            speakButton.isEnabled = true
+            stopButtton.isEnabled = false
+        }
+    }
 }
 
