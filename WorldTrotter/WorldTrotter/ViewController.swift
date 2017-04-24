79919400fe63ecba6701951786f59c7f41efa0cd
Amy Roberson <amy@roberson.xyz>
2017-04-24 14:39:17 -0400

    initial build of worldTrotter

---
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Amy Roberson on 4/24/17.
+//  Copyright © 2017 Amy Roberson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
