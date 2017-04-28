58024181237cd612f66c5a01b2feb35e5f2474e3
Amy Roberson <amy@roberson.xyz>
2017-04-27 20:24:01 -0400

    set up map view

---
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Amy Roberson on 4/27/17.
+//  Copyright © 2017 Amy Roberson. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+}
